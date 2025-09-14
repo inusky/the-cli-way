752d5c8d887ba233ce7662141d49cbf6a1ad8bf9
inusky <inuskydotcom@gmail.com>
2025-09-14 11:32:24 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-</template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+<script setup lang="ts">
+// TODO: Write some code
 </script>
 
-<style>
+<template></template>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
